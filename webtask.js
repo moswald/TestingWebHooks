566a9d9d9f703acf39d12237102e9793b21ec1b4
Matt Oswald <m@oswald.io>
2016-05-24 22:28:32 -0500

    adding first version

---
+++ webtask.js
@@ -0,0 +1,25 @@
+var azure = require('azure')
+
+module.exports = function (context, callback) {
+    var serviceBusService = azure.createServiceBusService('Endpoint=sb://moswald.servicebus.windows.net/;SharedAccessKeyName=WebTaskSender;SharedAccessKey=VsxilpfsoGvh8L+n7VPQZwbCw07PV5FEyjo9uK8vEHg=;');
+
+    var msg = {
+        body: context.webhook,
+        customProperties: {
+            extra: context.data
+        }
+    };
+
+    serviceBusService.sendQueueMessage(
+        'webhooks',
+        msg,
+        e =>
+        {
+            if (e) {
+                callback(e, '');
+            }
+            else {
+                callback(null, 'success');
+            }
+        });
+}
