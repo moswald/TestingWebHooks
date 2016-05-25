04dfe1f2a17c67f0fe91df996962fea5c5d4f54b
Matt Oswald <m@oswald.io>
2016-05-24 23:17:43 -0500

    removing logs, fixing message

--- webtask.js
+++ webtask.js
@@ -1,34 +1,24 @@
 ﻿var azure = require('azure')
 
 return function (context, callback) {
-    console.log("1");
     var serviceBusService = azure.createServiceBusService('Endpoint=sb://moswald.servicebus.windows.net/;SharedAccessKeyName=WebTaskSender;SharedAccessKey=VsxilpfsoGvh8L+n7VPQZwbCw07PV5FEyjo9uK8vEHg=;');
 
-    console.log("2");
-
     var msg = {
-        body: 'context.webhook',
+        body: context.webhook,
         customProperties: {
-            extra: 'context.data'
+            extra: context.data
         }
     };
 
-    console.log("3");
-
-     serviceBusService.sendQueueMessage(
-         "webhooks",
-         msg,
-         function(e) {
-             console.log("4");
-             if (e) {
-                 console.log("5");
-                 callback(e, '');
-             }
-             else {
-                 console.log("6");
-                 callback(null, 'success');
-             }
-         });
-
-    console.log("7");
+    serviceBusService.sendQueueMessage(
+        "webhooks",
+        msg,
+        function(e) {
+            if (e) {
+                callback(e, 'Failed to enqueue message');
+            }
+            else {
+                callback(null, 'success');
+            }
+        });
 }
